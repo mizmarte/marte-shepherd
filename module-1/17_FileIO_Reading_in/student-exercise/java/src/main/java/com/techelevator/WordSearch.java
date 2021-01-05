e633fb94ecde1d02395fcab58cf48f243d327d19
Marte Shepherd <blootaz@rocketmail.com>
2021-01-05 08:41:16 -0500

    Module 1 Day 17 wordSearch completed

--- module-1/17_FileIO_Reading_in/student-exercise/java/src/main/java/com/techelevator/WordSearch.java
+++ module-1/17_FileIO_Reading_in/student-exercise/java/src/main/java/com/techelevator/WordSearch.java
@@ -1,8 +1,68 @@
 package com.techelevator;
 
-public class WordSearch {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
-	public static void main(String[] args) {
+public class WordSearch 
+{
+
+	public static String getUserInput(String input)
+	{
+		Scanner scanner = new Scanner(System.in);
+		System.out.println(input);
+		String userInput = scanner.nextLine();
+		
+		return userInput;
+	}
+	
+	public static void displayOutput(String output)
+	{
+		System.out.println(output);
+	}
+	
+	
+	public static void main(String[] args) 
+	{
+		String filePath = getUserInput("What is the file that should be searched?");
+		String word = getUserInput("What is the search word you are looking for?");
+		String search = getUserInput("Should the search be case sensitive? (Y/N)");
+		File wordFile = new File(filePath);
+		Scanner scanner;
+		
+		try
+		{
+			scanner = new Scanner(wordFile.getAbsoluteFile());
+			int counter = 1;
+			while (scanner.hasNextLine())
+			{
+				String wordLine = scanner.nextLine();
+				String[] words = wordLine.split(" ");
+				for(String keyWord: words)
+				{
+					if (search.contentEquals("N"))
+					{
+						
+						if(keyWord.toLowerCase().contains(word.toLowerCase()))
+						{
+							displayOutput(counter + ")" + wordLine);
+						}
+						else if(search.equals("Y"))
+						{
+							if(keyWord.contains(word))
+							{
+								displayOutput(counter + ")" + wordLine);
+							}
+						}
+					}
+				}
+				counter++;
+			}
+		}
+		catch (FileNotFoundException e)
+		{
+			
+		}
 	
 
 	}
