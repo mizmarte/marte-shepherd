b2cd08cf2113a4f25d552db1f4cd118c986043cd
Marte Shepherd <blootaz@rocketmail.com>
2021-01-16 19:40:17 -0500

    completed step 4 and 5 of the review exercises - the 6th query was incomplete

--- module-2/Week1_Review/student-exercise/postgres/step-4.sql
+++ module-2/Week1_Review/student-exercise/postgres/step-4.sql
@@ -1,11 +1,46 @@
 -- select the park name, campground name, open_from_mm, open_to_mm & daily_fee ordered by park name and then campground name
+SELECT p.name AS park_name
+        , c.name AS campground_name
+        , open_from_mm
+        ,open_to_mm
+        ,daily_fee
+FROM park AS p
+INNER JOIN campground AS C
+        ON c.park_id = p.park_id
+ORDER BY park_name
+        , campground_name;
+
 
 
 -- select the park name and the total number of campgrounds for each park ordered by park name
-
+SELECT p.name AS park_name
+        ,COUNT (c.campground_id) AS total_number_of_campgrounds
+      
+FROM park AS p
+INNER JOIN campground AS C
+        ON c.park_id = p.park_id
+GROUP BY park_name
+ORDER BY park_name;
 
 
 -- select the park name, campground name, site number, max occupancy, accessible, max rv length, utilities where the campground name is 'Blackwoods'
+SELECT p.name AS park_name
+        , c.name AS campground_name
+        , s.site_number
+        , s.max_occupancy
+        , s.accessible
+        , s.max_rv_length
+        , s.utilities
+FROM park AS p
+INNER JOIN campground AS C
+        ON c.park_id = p.park_id
+INNER JOIN site AS s
+        ON s.campground_id = c.campground_id
+WHERE c.name ILIKE 'black%';
+        
+
+
+
 
 /*
   select park name, campground, total number of sites (column alias 'number_of_sites') ordered by park
@@ -20,9 +55,30 @@
   -------------------------------------------------
 */
 
+SELECT p.name AS park_name
+        , c.name AS campground_name
+        , COUNT (s.site_id) AS number_of_sites
+FROM park AS p
+INNER JOIN campground AS C
+        ON c.park_id = p.park_id
+INNER JOIN site AS s
+        ON s.campground_id = c.campground_id
+GROUP BY park_name
+        , campground_name
+ORDER BY park_name;
+
+
+
 
 -- select site number, reservation name, reservation from and to date ordered by reservation from date
-
+SELECT s.site_number
+        , r.name AS reservation_name
+        , r.from_date AS reservation_from_date
+        , r.to_date AS reservation_to_date
+FROM site AS s
+INNER JOIN reservation AS r
+ON r.site_id = s.site_id
+ORDER BY reservation_from_date;
 
 
 /*
@@ -38,6 +94,14 @@
   -------------------------------------------------
 */
 
-
+SELECT c.name AS campground_name
+        ,COUNT (r.reservation_id) AS total_number_reservations
+FROM campground AS c
+INNER JOIN site AS s
+        ON s.campground_id = c.campground_id
+INNER JOIN reservation AS r
+ON r.site_id = s.site_id
+GROUP BY campground_name
+ORDER BY total_number_reservations DESC;
 
 
--- module-2/Week1_Review/student-exercise/postgres/step-5.sql
+++ module-2/Week1_Review/student-exercise/postgres/step-5.sql
@@ -11,11 +11,32 @@
 */
 
 
+INSERT INTO park
+(
+        name, location, establish_date, area, visitors, description
+)
+VALUES
+(
+        'Ohiopyle State Park', 'Pennsylvania', '1965-01-01', 19052, 1000000, 'Ohiopyle State Park is a Pennsylvania state park on 19,052 acres in Dunbar, Henry Clay and Stewart Townships, Fayette County, Pennsylvania in the United States. The focal point of the park is the more than 14 miles of the Youghiogheny River Gorge that passes through the park.'
+);
+
+
+
+
 /*
   STEP TWO: You just found out that there was an error with the park data. Please update the park visitors to 1.5 million anually.
 
 */
 
+
+UPDATE park
+SET visitors = 1500000
+WHERE name ILIKE 'ohiop%';
+
+
+SELECT *
+FROM park
+WHERE location ILIKE 'penn%';
 
 /*
  STEP THREE: Insert new campground with the following data:
@@ -28,6 +49,17 @@
   ------------------------------------------------------------
 */
 
+INSERT INTO campground
+(
+        park_id, name, open_from_mm, open_to_mm, daily_fee
+)
+VALUES
+(
+        4, 'Youghiogheny', 01, 12, 95
+);
+
+SELECT name
+FROM campground;
 
 /*
  STEP FOUR: Insert 3 new sites with the following data:
@@ -41,6 +73,19 @@
 
 */
 
+INSERT INTO site
+(
+        site_number, campground_id
+)
+VALUES
+        (623,8)
+        ,(624,8)
+        ,(625,8);
+
+SELECT *
+FROM site
+WHERE campground_id = 8;
+
 
 /*
  STEP FIVE: Insert 3 reservations, 1 for each site with the following data:
@@ -52,15 +97,48 @@
 
 */
 
+INSERT INTO reservation
+(
+        site_id, name, from_date, to_date
+)
+VALUES
+        (623, 'Wayne Family',(select current_date + interval '10 days') , (select current_date + interval '20 days'))
+       ,(624, 'Parker Family',(select current_date + interval '11 days'), (select current_date + interval '20 days'))
+       ,(625, 'Kent Family',(select current_date + interval '12 days'), (select current_date + interval '20 days'));
+        
 
+SELECT *
+FROM reservation
+ORDER BY reservation_id DESC;
 /*
  STEP SIX: The Wayne Family called and asked if they could change their reservation to today. Update the from_date to today and the to_date to a week from today.
 
  */
+BEGIN TRANSACTION;
 
+UPDATE reservation
+SET
+(
+       -- from_date = select current_date
+       -- , to_date = select current_date + interval '7 days' ERROR MESSAGE AROUND EQUAL SIGN.....POSTGRESQL DOES NOT LIKE THE FORMAT OF THESE LINES
+)
 
+WHERE site_id = 623;
+        
+
+ 
+ROLLBACK;
 /*
  STEP SEVEN: The Kent family called and they would like to cancel their reservation. Delete the reservation for Kent Family.
 
 */
+BEGIN TRANSACTION;
 
+DELETE FROM reservation
+WHERE site_id = 625;
+
+ROLLBACK;
+
+SELECT*
+FROM reservation
+ORDER BY site_id DESC;
