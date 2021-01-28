15f1fae7e6e30be794586cdcaf0766d6a3557ddb
Marte Shepherd <blootaz@rocketmail.com>
2021-01-27 22:24:45 -0500

    have completed some of Module 2 Day 11 Student Exercise

--- module-2/11_ServerSide_APIs_Part_1/student-exercise/java/auctions/src/main/java/com/techelevator/auctions/controller/AuctionController.java
+++ module-2/11_ServerSide_APIs_Part_1/student-exercise/java/auctions/src/main/java/com/techelevator/auctions/controller/AuctionController.java
@@ -9,12 +9,57 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/auctions")
-public class AuctionController {
+public class AuctionController 
+{
 
-    private AuctionDAO dao;
+	private AuctionDAO dao;
 
-    public AuctionController() {
-        this.dao = new MemoryAuctionDAO();
+	public AuctionController() 
+	{
+		this.dao = new MemoryAuctionDAO();
+	}
+
+	@RequestMapping(method = RequestMethod.GET)
+	public List<Auction> list() 
+	{
+        return dao.list();
+        
     }
-
+	
+	@RequestMapping(path = "/{id}", method = RequestMethod.GET)
+	public Auction get(@PathVariable int id)
+	{
+		return dao.get(id);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public void create(@RequestBody Auction auction)
+	{
+		dao.create(auction);
+	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public  List<Auction> list
+	(@RequestParam(value="title_like", defaultValue = "") String title)
+	{
+		if (title.length() > 0)
+		{
+			return dao.searchByTitle(title);
+		}
+		return dao.list();
+		
+	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public  List<Auction> list
+	(@RequestParam(value="currentBid_lte", defaultValue = "0") double currentBid_lte) 
+	{
+		if (currentBid_lte > 0)
+		{
+			return dao.searchByPrice(currentBid_lte);
+		}
+		return dao.list();
+		
+	}
+	
 }
