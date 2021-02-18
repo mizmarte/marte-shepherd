44c76c6abaf170c4b5049a080ee9af629b012674
Marte Shepherd <blootaz@rocketmail.com>
2021-02-17 19:22:40 -0500

    student exercise completed

--- module-3/22_DOM/student-exercise/shopping-list/js/shopping-list.js
+++ module-3/22_DOM/student-exercise/shopping-list/js/shopping-list.js
@@ -1,8 +1,8 @@
 // add pageTitle
   const pageTitle = 'My Shopping List'
 // add groceries
-  const groceries = [
-    {'turkey bacon'},{'eggs'},{'shrimp'},{'lump crabmeat'},{'paper plates'},{'bleach'},{'honey granola cookies'},{'bananas'},{'spinach'},{'fresh mango'}];
+  let groceries = [
+    'turkey bacon','eggs','shrimp','lump crabmeat','paper plates','bleach','honey granola cookies','bananas','spinach','fresh mango'];
 /**
  * This function will get a reference to the title and set its text to the value
  * of the pageTitle variable that was set above.
@@ -11,20 +11,36 @@ function setPageTitle()
 {
    const title = document.getElementById('title');
    title.innerHTML = pageTitle;
-
-
 }
 
 /**
  * This function will loop over the array of groceries that was set above and add them to the DOM.
  */
-function displayGroceries() {}
+function displayGroceries() 
+{
+  const parentContainer = document.getElementById('groceries');
+  groceries.forEach((item) => {
+    const li = document.createElement('li')
+    li.innerText = item;
+    parentContainer.appendChild(li);
+  })
+  
+}
 
 /**
  * This function will be called when the button is clicked. You will need to get a reference
  * to every list item and add the class completed to each one
  */
-function markCompleted() {}
+function markCompleted() 
+{
+  const parentContainer = document.getElementById('groceries');
+  const item = parentContainer.children;
+  for (let i = 0; i < item.length; i++)
+  {
+    item[i].setAttribute('class','completed');
+  };
+
+}
 
 setPageTitle();
 
